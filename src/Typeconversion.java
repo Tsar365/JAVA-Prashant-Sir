d80668d06a20f7994d21fa221e728af4a21d206f
Ehasanul Haque <ehasanmd365@gmail.com>
2026-05-16 08:41:43 +0600

    Added Function

---
+++ src/Typeconversion.java
@@ -0,0 +1,9 @@
+public class Typeconversion {
+    public static void main(String[] args) {
+        float myFloat=5;
+        System.out.println(myFloat);
+
+        int myInt=(int) 5.4f;
+        System.out.println(myInt);
+    }
+}
